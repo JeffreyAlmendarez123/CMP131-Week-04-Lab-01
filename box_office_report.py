9d38895283f8791ccb4958625154100e5d1672d7
almendarez.jeffre <almendarez.jeffre@student.ccm.edu>
2026-09-16 09:06:25 -0400

    complete Week 4 lab 1

--- box_office_report.py
+++ box_office_report.py
@@ -1,19 +1,17 @@
 movie_name=input("Name of Movie: ")
 adult_tickets=int(input("How much Adults tickets were sold? "))
 child_tickets= int(input("How many childrens tickets were sold? "))
-adult_price=10
-child_price=6
-theater_percantage=0.2
+adult_price=adult_tickets * 10.00
+child_price=child_tickets * 6.00
+theater_percantage=0.20
 gross_profit=(adult_tickets * adult_price)+ (child_tickets * child_price)
 net_profit=gross_profit*theater_percantage
-distributor_amount=gross_profit-net_profit
+distributor_amount=gross_profit-net_profit 
 print(movie_name)
 print(adult_tickets)
 print(child_tickets)
 print()
-print(adult_price)
-print(child_price)
-print(theater_percantage)
-print(gross_profit)
-print(net_profit)
-print(distributor_amount)
+
+print(f"Gross Box profit:   ${gross_profit:,.2f}")
+print(f"Net box profit:  ${net_profit:,.2f}")
+print(f"Amount paid to distrubutor: ${distributor_amount:,.2f}")
